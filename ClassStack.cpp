dd2b5097a7c5a4525be348316d7b85bb44950397
kk87lk <786279458@qq.com>
2018-03-26 22:31:20 +0800

    new

---
+++ ClassStack.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <malloc.h>
+using namespace std;
+
+typedef int ElementType;
+
+struct SNode
+    {
+      ElementType *Data;
+      int MaxSize;
+      int Top;
+    };
+typedef struct SNode *StackList;
+
+class Stack
+{
+private:
+    bool CheckFull(StackList L)
+    {
+        return (L->Top == L->MaxSize - 1);
+    }
+    bool CheckEmpty(StackList L)
+    {
+        return (L->Top == -1);
+    }
+public:
+    static StackList creat(int MaxSize)
+    {
+        StackList L = (StackList)malloc(sizeof(struct SNode));
+        L->Data = (ElementType *)malloc(MaxSize * sizeof(ElementType));
+        L->Top = -1;
+        L->MaxSize = MaxSize;
+        return L;
+    }
+
+    bool Push(StackList L, ElementType X)
+    {
+        if (CheckFull(L))
+        {
+            cout << "Full" << endl;
+            return false;
+        }
+        else
+        {
+            L->Data[++(L->Top)] = X;
+            return true;
+        }
+    }
+
+    ElementType Pop (StackList L)
+    {
+        if (CheckEmpty(L))
+        {
+            cout << "Empty" << endl;
+            return 0;
+        }
+        else
+        {
+            return L->Data[(L->Top)--];
+        }
+    }
+};
+int main()
+{
+    Stack S;
+    StackList s = NULL;
+    int i = -1;
+    while (i != 0)
+    {
+        system("cls");
+        cout << "1--Creat one stacklist." << endl;
+        cout << "2--Push stack." << endl;
+        cout << "3--Pop stack&print." << endl;
+        cout << "0--Exit." << endl;
+        cin >> i;
+        switch(i)
+        {
+            case 1:
+            {
+                system("cls");
+                int Size;
+                cout << "Input how many elements you want to store into the Stack:";
+                cin >> Size;
+                s = S.creat(Size); //Maxsize == Size
+                cout << "Successful created.";
+                fflush(stdin);
+                getchar();
+                break;
+            }
+            case 2:
+            {
+                system("cls");
+                ElementType m;
+                cout << "Input the value you want to insert into the Stack:";
+                cin >> m;
+                S.Push(s, m); //Push Element-m into the Stack
+                cout << "Successful inserted.";
+                fflush(stdin);
+                getchar();
+                break;
+            }
+            case 3:
+            {
+                system("cls");
+                int value;
+                value = S.Pop(s);//Print the top data
+                cout << value << endl;
+                cout << "Successful outputed.";
+                fflush(stdin);
+                getchar();
+                break;
+            }
+            case 0:
+            {
+                return 0;
+            }
+            default:
+                system("cls");
+                cout << "Error option." << endl;
+                fflush(stdin);
+                getchar();
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
