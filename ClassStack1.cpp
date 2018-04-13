76745f49c2879392f9abaa5166b96e1950d23b28
Darry Tsui <786279458@qq.com>
2018-04-13 15:07:47 +0800

    1

--- ClassStack1.cpp
+++ ClassStack1.cpp
@@ -3,11 +3,9 @@
 
 using namespace std;
 
-typedef int ElementType;
-
 struct SNode
     {
-      ElementType *Data;
+      int *Data;
       int MaxSize;
       int Top;
     };
@@ -25,12 +23,12 @@ private:
         return (L->Top == -1);
     }
 public:
-    StackList creat(int MaxSize)
+    StackList creat()
     {
         StackList L = new struct SNode;
-        L->Data = new ElementType[MaxSize];
+        L->Data = new int[1000];
         L->Top = -1;
-        L->MaxSize = MaxSize;
+        L->MaxSize = 1000;
         return L;
     }
 
@@ -49,7 +47,7 @@ public:
         L->Top = re;
     }
 
-    bool Push(StackList L, ElementType X)
+    bool Push(StackList L, int X)
     {
         if (CheckFull(L))
         {
@@ -63,7 +61,7 @@ public:
         }
     }
 
-    ElementType Pop (StackList L)
+    int Pop (StackList L)
     {
         if (CheckEmpty(L))
         {
@@ -76,11 +74,78 @@ public:
         }
     }
 };
+
+void tran1022(int num)
+{
+    Stack S;
+    StackList s = NULL;
+    s = S.creat();
+    int Quotient = num, Remainder = -1;
+    while (Quotient == 0)
+    {   
+        Remainder = Quotient % 2;
+        S.Push(s, Remainder);
+        Quotient = Quotient / 2;
+    }
+    cout << "The number " << num << " has been transformed into (";
+    while (s->Top > -2)
+    {
+        cout << s->Data;
+    }
+    cout << ")B" << '\n';
+}
+
+void tran1028(int num)
+{
+    
+}
+
 int  main()
 {
-    int se = 3;
-    while (se == 3)
+    int i = -1;
+    while (i != 0)
     {
-        cout << "Input the function you want to choose:" << '\n';
-        cin << se;
+        system("cls");
+        cout << "1--10 to 2." << endl;
+        cout << "2--10 to 8." << endl;
+        cout << "0--Exit." << endl;
+        cin >> i;
+        switch(i)
+        {
+            case 1:
+            {
+                system("cls");
+                int num;
+                cout << "Input the number:";
+                cin >> num;
+                tran1022(num);
+                cout << "Successful transformed.";
+                fflush(stdin);
+                getchar();
+                break;
+            }
+            case 2:
+            {
+                system("cls");
+                int num;
+                cout << "Input the number:";
+                cin >> num;
+                //function 8
+                cout << "Successful transformed.";
+                fflush(stdin);
+                getchar();
+                break;
+            }
+            case 0:
+            {
+                return 0;
+            }
+            default:
+                system("cls");
+                cout << "Error option." << endl;
+                fflush(stdin);
+                getchar();
+                break;
+        }
+    }
 }
