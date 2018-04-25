4b498605ccf4cf83d354f4886b37f9559e543dcf
Darry Tsui <786279458@qq.com>
2018-04-25 21:57:39 +0800

    1

---
+++ string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#define ElementType int
+
+using namespace std;
+
+class str
+{
+    private:
+
+    public:
+        char *search(char *MainStr, char *SubStr)
+        {
+
+        }
+        void replace(char *MainStr, char *StrToBeReplaced, char *StrToReplace)
+        {
+
+        }
+        void insert(char *MainStr, char *StrToBeInserted)
+        {
+
+        }
+        char *takeout(char *MainStr, int n)
+        {
+            
+        }
+};
