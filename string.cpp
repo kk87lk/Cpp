f1e0651a604e566f4ce880c77b022d09d332539e
Darry Tsui <786279458@qq.com>
2018-05-06 19:46:58 +0800

    1

--- string.cpp
+++ string.cpp
@@ -7,13 +7,11 @@ using namespace std;
 
 class str
 {
-    private:
-
     public:
         int match(string MainStr, string SubStr, int pos)
         {
             int i = pos - 1, j = 0;
-            int ml = MainStr.length, sl = SubStr.length;
+            int ml = MainStr.length(), sl = SubStr.length();
             while( i < ml && j < sl)
             {
                 if (MainStr[i] == SubStr[j])
@@ -34,21 +32,31 @@ class str
         }
         void replace(string MainStr, string StrToBeReplaced, string StrToReplace)
         {
-            int ml = MainStr.length, sl = SubStr.length;
+            int ml = MainStr.length(), sl = StrToBeReplaced.length(), sl2 = StrToReplace.length();
             queue<int> q1(ml);
             int reco;
             for (int i = 1; i <= ml - sl; i++)
             {
                 reco = match(MainStr, StrToBeReplaced, i);
-                if ( reco != 0)
-                    {
-                        q1.push(reco);
-                        i = i + sl - 1;
-                    }
+                if ( reco != -1)
+                {
+                    q1.push(reco - 1);  //存入下标
+                    i = i + sl - 1;
+                }
                 else 
+                {
+                    if (sl >= sl2)
+                    {
+
+                    }
+                    else
+                    {
+                        
+                    }
+                }
             }
         }
-        void insert(string MainStr, string StrToBeInserted)
+        void insert(string MainStr, string StrToBeInserted, int pos)
         {
             
         }
--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-public:
-        Queue *Creat(Queue* Q, int MaxSize)
-        {
-            Q = (Queue *)malloc(MaxSize * sizeof(Queue));
-            return Q;
-        }
