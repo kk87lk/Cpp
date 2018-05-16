0c2b1aa15b1b4872edcefa0221e5ddcae58f8dc7
Darry Tsui <786279458@qq.com>
2018-05-16 15:20:22 +0800

    1

--- 1.4.4if.cpp
+++ 1.4.4if.cpp
@@ -1,21 +1,65 @@
-#include "iostream"
+#include <stdio.h>
+#include <string.h>
+/*  函数声明 */
+int index(char *s, char *t, int pos);
+void replace(char *s, char *t, char *v);
+/*  主函数 */
 int main()
 {
-    int currVal = 0, val = 0;
-    if (std::cin >> currVal)
+    int k;
+    char *s, t[] = "abc", *v = "%%%";
+    s = (char *)malloc(100 * sizeof(char));
+    printf("\n s=?");
+    gets(s);
+    printf("\n s=%s\n", s);
+    printf("\n t=%s    v=%s\n", t, v);
+    replace(s, t, v); /* 调用串置换函数 */
+    printf("\n\n new string=%s\n", s);
+} /* main   */
+/*  串的置换，将主串s中的t串，置换为v串  */
+void replace(char *s, char *t, char *v)
+{
+    int i, j, k, po, sl, tl;
+    sl = strlen(s);
+    tl = strlen(t);
+    printf("\n sl=%d   tl=%d\n", sl, tl);
+    po = 0;
+    while (po < sl - tl + 1)
     {
-        int cnt = 1;
-        while (std::cin >> val)
+        k = index(s, t, po); /* 调用串匹配函数 */
+        printf("\n k=%2d", k);
+        i = k - 1;
+        for (j = 0; j <= tl - 1; j++)
         {
-            if (val == currVal)
-                ++cnt;
-            else
-            {
-                std::cout << currVal << " occurs " << cnt << "times" << std::endl;
-                currVal = val;
-                cnt = 1;
-            }
+            s[i] = v[j];
+            i++;
         }
-        std::cout << currVal << " occurs " << cnt << "times" <<std::endl;
+        po = k + tl;
+        printf("     pos=%2d", po);
     }
-}
+} /*  replace */
+/*  串匹配函数 */
+/*  从主串s的第pos个字符开始查找子串t，函数结果是子串t在主串s的pos开始之后首次出现的位置 */
+int index(char *s, char *t, int pos)
+{
+    int i, j, sl, tl;
+    i = pos;
+    j = 1;
+    sl = strlen(s);
+    tl = strlen(t);
+    while (i <= sl && j <= tl)
+        if (s[i - 1] == t[j - 1])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            i = i - j + 1 + 1;
+            j = 1;
+        }
+    if (j > tl)
+        return (i - tl);
+    else
+        return (-1);
+} /*  index  */
--- string.cpp
+++ string.cpp
@@ -1,77 +1,202 @@
 #include <cstdlib>
 #include <iostream>
 #include <queue>
-#include <string>
+//#include <string>
 
 using namespace std;
 
-class str
+class String
 {
   public:
-    int match(string MainStr, string SubStr, int pos)
+    struct String1
+    {
+        char String[1000] = {'\0'};
+    } unit;
+    int length()
+    {
+        int i = 0;
+        for (; unit.String[i] != 0; i++)
+            ;
+        return i;
+    }
+};
+class str
+{
+  private:
+    int match(String MainStr, String SubStr, int pos)
     {
         int i = pos - 1, j = 0;
         int ml = MainStr.length(), sl = SubStr.length();
         while (i < ml && j < sl)
         {
-            if (MainStr[i] == SubStr[j])
+            if (MainStr.unit.String[i] == SubStr.unit.String[j])
             {
                 i++;
                 j++;
             }
             else
             {
-                i = i - j + 2;
-                j = 1;
+                i = i + 1;
+                j = 0;
             }
         }
-        if (j = sl)
+        if (j == sl)
             return (i - sl + 1);
         else
             return -1;
     }
-    void replace(string MainStr, string StrToBeReplaced, string StrToReplace)
+
+  public:
+    void replace(String *MainStr, String StrToBeReplaced, String StrToReplace)
     {
-        int ml = MainStr.length(), sl = StrToBeReplaced.length(), sl2 = StrToReplace.length();
-        queue<int> q1(ml);
-        int reco, mark;
-        for (int i = 1; i <= ml - sl; i++)
+        int ml = MainStr->length(), sl = StrToBeReplaced.length(), sl2 = StrToReplace.length();
+        int reco, mark = 0;
+        if (sl >= sl2)
         {
-            reco = match(MainStr, StrToBeReplaced, i);
-            if (reco != -1)
+            for (int i = 1; i <= ml - sl;)
             {
-                q1.push(reco - 1); //存入下标
-                i = i + sl - 1;
-            }
-            else
-            {
-                if (sl >= sl2)
+                int flag = 0, rip = 0;
+                while (1)
                 {
-                    while (!q1.empty())
-                    {
-                        mark = q1.front();
-                        q1.Pop();
-                        for (int count = 0; count < sl2; count++)
-                            MainStr[mark + count] = StrToReplace[count];
-                        for (count = 0; count < ml - mark - sl; count++)
-                            MainStr[mark + count + sl2] = MainStr[mark + count + sl2 + 1];
-                        if (q1.empty())
-                            return;
-                        mark = q1.front();
-                        q1.Pop();
-                        q1.Push(mark - sl + sl2);
-                    }
+                    mark = match(*MainStr, StrToBeReplaced, i);
+                    if (mark == -1)
+                        return;
+                    for (int count = 0; count < sl2; count++)
+                        (*MainStr).unit.String[mark + count - 1] = StrToReplace.unit.String[count];
+                    for (int count = 0; count < ml - mark - sl + 2; count++)
+                        (*MainStr).unit.String[mark + count + sl2 - 1] = (*MainStr).unit.String[mark + count + sl - 1];
+                    i = i + sl2 + 1;
                 }
-                else
+            }
+        }
+        else
+        {
+            for (int i = 1; i <= ml - sl;)
+            {
+
+                int flag = 0, rip = 0;
+                while (1)
                 {
+                    ml = MainStr->length();
+                    mark = match(*MainStr, StrToBeReplaced, i) - 1;
+                    if (mark == -2)
+                        return;
+                    for (int count = 0; count < ml - mark + sl2 - (sl * 2) - 1; count++)
+                        (*MainStr).unit.String[ml - count + sl2 - sl - 1] = (*MainStr).unit.String[ml - count - 1];
+                    for (int co = 0; co < sl2; co++)
+                        (*MainStr).unit.String[mark + co] = StrToReplace.unit.String[co];
+                    i = i + sl;
                 }
             }
         }
     }
-    void insert(string MainStr, string StrToBeInserted, int pos)
+    void insert(String *MainStr, String StrToBeInserted, int pos)
     {
+        int len = (*MainStr).length();
+        int ml = MainStr->length();
+        int sl = StrToBeInserted.length();
+        for (int i = 0; i < len - pos; i++)
+            (*MainStr).unit.String[ml + sl - 1 - i] = (*MainStr).unit.String[ml - i - 1];
+        for (int i = 0; i < sl; i++)
+            (*MainStr).unit.String[pos + i] = StrToBeInserted.unit.String[i];
+        cout << "Successful insert.";
+        system("pause");
+        return;
     }
-    char *takeout(string MainStr, int n)
+    char takeout(String MainStr, int pos, int num)
     {
+        char SubStr[100];
+        for (int i = 0; i < num; i++)
+            SubStr[i] = MainStr.unit.String[pos + i];
+        return *SubStr;
     }
 };
+
+int main()
+{
+    String a;
+    String b;
+    String c;
+    cin >> a.unit.String;
+    cin >> b.unit.String;
+    // cin >> c.unit.String;
+
+    s.insert(&a, b, 5);
+    cout << "\n"
+         << a.unit.String;
+
+    int op = -1;
+    while (op != 0)
+    {
+        system("cls");
+        cout << "1-Replace." << endl;
+        cout << "2-Insert." << endl;
+        cout << "3-Get SubString." << endl;
+        cout << "0-exit." << endl;
+        cin >> op;
+        switch (op)
+        {
+        case 0:
+            return -1;
+        case 1:
+        {
+            String a;
+            String b;
+            String c;
+            str s;
+            system("cls");
+            cout << "Type in the main string:";
+            cin >> a.unit.String;
+            cout << "\nType in the string you want to be replaced:";
+            cin >> b.unit.String;
+            cout << "\nType in the string to replace it:";
+            cin >> c.unit.String;
+            s.replace(&a, b, c);
+            cout << "\nReplace success!\nThe main string now is:";
+            cout << a;
+            break;
+        }
+        case 2:
+        {
+            String a;
+            String b;
+            String c;
+            str s;
+            system("cls");
+            cout << "Type in the main string:";
+            cin >> a.unit.String;
+            int pos;
+            cout << "\nType in the position you want to insert:";
+            cin >> pos;
+            cout << "\nType in the string to insert:";
+            cin >> b.unit.String;
+            s.insert(&a, b, pos);
+            cout << "\nReplace success!\nThe main string now is:";
+            cout << a;
+            break;
+        }
+        case 3:
+        {
+            String a;
+            String b;
+            String c;
+            str s;
+            system("cls");
+            cout << "Type in the main string:";
+            cin >> a.unit.String;
+            int pos;
+            cout << "\nType in the position of string you want to takeout:";
+            cin >> pos;
+            int num;
+            cout << "\nType in how many types you want to takeout:";
+            cin >> num;
+            b.unit.String = s.takeout(&a, pos, num);
+            cout << "\nReplace success!\nThe main string now is:";
+            cout << b.unit.String;
+            break;
+        }
+        default:
+            cout << "Wrong option.";
+        }
+    }
+}
